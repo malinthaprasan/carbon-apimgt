e5c19ceea0552b8a01c5288d9b6ba97d6ef4820e
Malintha Amarasinghe <malintha.prasan@gmail.com>
2021-04-04 01:04:06 +0530

    change actions

---
+++ .github/scripts/testngFailedParser/testngFailedParser.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+    xml2js = require('xml2js');
+ 
+var parser = new xml2js.Parser();
+
+console.log("::set-output name=failedTests::hello,123")
+
+//fs.readFile('product-apim/modules/integration/tests-integration/tests-backend/target/surefire-reports/testng-failed.xml', function(err, data) {
+//    parser.parseString(data, function (err, result) {
+//        result.suite.test.forEach((test) => {
+//            test.classes[0].class.forEach((cls) => {
+//                console.log(cls['$'].name);
+//            });
+//        });
+//    });
+//});
